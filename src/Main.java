b92b3dee1c1bbd168cf50577d22a6103edcadbd9
lucas rehn <lucasr1234@hotmail.com>
2016-10-23 01:04:51 +0200

    added: sex variables, name variables, dmg & hp variables.

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,11 @@ import java.util.Scanner;
 
 public class Main {
 
+	Scanner scanner = new Scanner(System.in);
 	boolean isRunning = true;
-	
+	String name;
+	String sex;
+	Player player;
 	
 	public Main(){
 		init();
@@ -15,6 +18,23 @@ public class Main {
 	}
 	
 	public void init(){
+		
+		System.out.println("Welcome svantish rpg!");
+		System.out.print("Tell me, are you a boy or a girl?: ");
+		
+		sex = scanner.nextLine();
+		
+		System.out.print("That's nice! Now please tell me, what is your name?: ");
+		
+		name = scanner.nextLine();
+		
+		if(sex.equals("boy")){
+			player = new Player(100, 15, name, sex);
+		}else{
+			player = new Player(1, 10, name, sex);
+		}
+		
+		
 		
 	}
 	
