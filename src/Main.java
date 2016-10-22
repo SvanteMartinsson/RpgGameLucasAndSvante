e7455d6619d9fe464fa1cc7eb71dcfc07a00fe98
lucas rehn <lucasr1234@hotmail.com>
2016-10-23 00:29:30 +0200

    added prLn & scanner

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("hello world");
+		Scanner scanner = new Scanner(System.in);
 
 	}
 
